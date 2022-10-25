4b55e0ab4ae8c6b4d12c323fef2e70894ff762f5
Elisabeth2703 <polskayaeliza@gmail.com>
2022-10-25 12:17:58 +0200

    add exception

--- exception.h
+++ exception.h
@@ -9,3 +9,91 @@ public:
 };
 
 #endif // EXCEPTION_H
+// exception.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+//исключения регистрация
+bool Data(std::ifstream& Reg, std::string filename) {
+    sizeof count = 0;
+    char name;
+
+    while (Reg >> name)
+    {
+        if (name != '.')
+            if (!isdigit(name)) // если имя написано, тогда попадает в базу данных
+                count++; //увеличение имен в базе данных
+            else {
+                cout << "Ошибка! Вы должны ввести имя пользователя.";
+            }
+    }
+    if (count)
+        return false;
+        cout << "Данное имя уже занято другим пользователем, выбeрите новое имя.";
+    else
+        return true;
+
+}
+
+string CorrectPassword(int length) {
+        const string alphabet =
+            "0123456789"
+            "!@#$%^&*"
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz";
+        string password;
+
+        for (int i = 0; i < length; ++i)
+            password += alphabet[rand() % alphabet.size()];
+
+        return password;
+    }
+
+void readValue(int& value) {
+        while (!(cin >> value) || (cin.peek() != '\n') || (value <= 0)) {
+            cin.clear();
+
+            while (cin.get() != '\n');
+                cout << "Ошибка! Вводить можно только положительные числа!" << endl;
+        }
+}
+
+//исключение вход
+bool InPutt {
+    string names;
+    string passwords;
+
+    cout << "Введите логин" << endl;
+    cin >> names;
+    cout << "Введите пароль" << endl;
+    cin >> passwords;
+
+    if (names = _name && passwords = _password) {
+        return true;
+    else
+        return false;
+    cout << "Введен неправильный логин или пароль. Повторите попытку.";
+    }
+}
+
+//исключения кнопки
+
+void Buttons{
+
+    try
+    {
+        if (gameball != 0,2 || gameball != 0,4 || gameball != 0,6)
+            throw "Вы не выбрали размер мяча, пожалуйста повторите попытку.";
+    }
+    catch (const char* exception)
+    {
+        std::cerr << "Ошибка: " << exception << '\n';
+    }
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+/
