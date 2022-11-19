4ef0cc79abdcf83a1f565829ec0fe78512476375
Александра Зивер <91556009+DARKSWAAN@users.noreply.github.com>
2022-11-19 15:23:13 +0200

    Revert "Destiny calc game scene (#27)"
    This reverts commit 68ffd894104f693aebff22c6867771423502ad47.

--- bonus.cpp
+++ bonus.cpp
@@ -7,37 +7,9 @@ extern int plr_1_HeightJump;
 extern int plr_2_Speed;
 extern int plr_2_HeightJump;
 
-extern bool bonusCreated;
-
-
 extern qreal toB2(qreal value);
 
 Bonus::Bonus(int xspread) : QGraphicsEllipseItem(0)
-
-{
-    int bonusTypeRand = rand() % 3 +1;
-    QColor color = Qt::black;
-
-    if(bonusTypeRand == 1) {
-        color = Qt::yellow;
-        bonusType = "speedBonus";
-    }
-    else if(bonusTypeRand == 2) {
-        color = Qt::green;
-        bonusType = "jumpBonus";
-    }
-    else if(bonusTypeRand == 3) {
-        color = Qt::red;
-        bonusType = "negativeSpeedBonus";
-    }
-
-    setBrush(color);
-    setRect(0, 0, 50, 50);
-    setPos(rand() % (xspread - 50), 0);
-}
-
-void Bonus::advance(int phase)
-
 {
     int bonusTypeRand = rand() % 3 +1;
     QColor color = Qt::black;
@@ -103,52 +75,7 @@ void Bonus::advance(int phase)
                 plr_2_Speed = 2;
             }
 
-
-    if(phase) {
-        moveBy(0, yspeed);
-        QPointF pos = gameBonus->pos();
-        if(toB2(pos.y()) > 5.5) {
             delete this;
-            return;
-        }
-
-
-        if(data(0).toBool()) {                          // player 1
-            if(bonusType == "speedBonus")
-            {
-                plr_1_Speed = 13;
-            }
-            else if(bonusType == "jumpBonus")
-            {
-                plr_1_HeightJump = 10;
-            }
-            else if(bonusType == "negativeSpeedBonus")
-            {
-                plr_1_Speed = 2;
-            }
-
-            delete this;
-            bonusCreated = false;
-        }
-
-        if(data(1).toBool()) {                          // player 2
-            if(bonusType == "speedBonus")
-            {
-                plr_2_Speed = 13;
-            }
-            else if(bonusType == "jumpBonus")
-            {
-                plr_2_HeightJump = 10;
-            }
-            else if(bonusType == "negativeSpeedBonus")
-            {
-                plr_2_Speed = 2;
-            }
-            delete this;
-            bonusCreated = false;
-
-            delete this;
-
         }
     }
 }
