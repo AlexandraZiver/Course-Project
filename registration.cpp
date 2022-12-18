462851793ba31a513d6d30e2c9f5b3aba034795a
Elisabeth2703 <polskayaeliza@gmail.com>
2022-12-18 17:33:29 +0100

    add new class registration

--- registration.cpp
+++ registration.cpp
@@ -24,7 +24,59 @@ void registration::on_playNoReg_clicked()
 }
 
 
+void registration::on_reg_clicked()
+{
+    funct_registr();
 
+    QString playerName = ui->lineEdit_PlayerName->text();
+    QString password = ui->lineEdit_password->text();
+    QString passwordCheck = ui->lineEdit_passwordCheck->text();
+    QString login;
+    int errorCode = ExceptionOn_reg_clicked(playerName, password, passwordCheck);
+
+    Error *error = new Error(errorCode);
+    error->whatError();
+
+    switch (error->getErrorCode()) {
+    case 101:
+        ui->lineEdit_PlayerName->setText("");
+        return;
+
+    case 102:
+        ui->lineEdit_password->setText("");
+        ui->lineEdit_passwordCheck->setText("");
+        return;
+
+    case 103:
+        ui->lineEdit_password->setText("");
+        ui->lineEdit_passwordCheck->setText("");
+        return;
+
+    case 104:
+        ui->lineEdit_passwordCheck->setText("");
+        return;
+
+    case 106:
+        ui->lineEdit_PlayerName->setText("");
+        return;
+
+    case 107:
+        ui->lineEdit_password->setText("");
+        ui->lineEdit_passwordCheck->setText("");
+        return;
+
+    case 108:
+        return;
+
+    default:
+        break;
+    }
+
+    gamepreparation *auth;
+    auth = new gamepreparation;
+    auth->show();
+    this->close();
+}
 
 
 
